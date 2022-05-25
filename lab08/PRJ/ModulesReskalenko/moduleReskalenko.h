1064147c6841a746223bfc1e439372bae1b71baf
Reskalenko-Yaroslav-KB-21 <xxxx@zzzz.cccc>
2022-05-25 15:08:35 +0300

    Lab8

--- lab08/PRJ/ModulesReskalenko/moduleReskalenko.h
+++ lab08/PRJ/ModulesReskalenko/moduleReskalenko.h
@@ -4,11 +4,14 @@
 #include <cmath>
 #include <math.h>
 #include <iostream>
-
+int main (){
+  setlocale(LC_ALL,"");
+  return 0;
+}
 float s_calculation(float x, float y, float z){
     float S,p;
-     p = 3.14;
-   S = pow(x,2)-pow(y,3)+sqrt(pow(y,x)-p);
+    p = 3.14;
+    S = pow(x,2)-pow(y,3)+sqrt(pow(y,x)-p);
     return S;
 
 }
